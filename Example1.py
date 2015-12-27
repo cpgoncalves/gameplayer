59e7e2cd1018dd29e39ef5a7ac867b4248ab7442
Carlos Pedro dos Santos Gonçalves <cgon.aulas@gmail.com>
2015-12-27 20:23:39 +0000

    Create Example1.py

---
+++ Example1.py
@@ -0,0 +1,26 @@
+ Carlos Pedro Gonçalves (2015), Game Theory with Python
+# Game Theory and Applied A.I. Classes
+# Instituto Superior de Ciências Sociais e Políticas (ISCSP)
+# University of Lisbon
+# cgoncalves@iscsp.ulisboa.pt
+#
+# Two Person's Prisonners' Dilemma Game
+#
+# For more details see the user manual that comes with the package:
+# Gonçalves, C.P. (2015) "Game Player User Manual - A Game Theory Analyzer With Python",
+#     https://sites.google.com/site/autonomouscomputingsystems/game-player
+
+
+import gamep # import game player main module
+
+config = [] # setup the game configuration list
+
+# add the alternative game configurations:
+gamep.addAlternative(["Cooperate","Cooperate"],[2,2],config)
+gamep.addAlternative(["Cooperate","Defect"],[0,4],config)
+gamep.addAlternative(["Defect","Cooperate"],[4,0],config)
+gamep.addAlternative(["Defect","Defect"],[1,1],config)
+
+print("\nGame Configuration: ", config)
+
+gamep.analyzeGame(config,True) # analyze the game
